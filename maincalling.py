c10da51c48f7b2d8584f307dfa5a5715abf3335d
Kartikeya Srivastava <kartikeyasrivastava861@gmail.com>
2020-12-13 06:09:37 +0530

    Update maincalling.py

--- maincalling.py
+++ maincalling.py
@@ -10,23 +10,26 @@ phone = StringVar()
 address = StringVar()
 conn = sqlite3.connect("bus.db")
 c = conn.cursor()
-# c.execute("DROP TABLE DRIVER")
-# driverd = '''CREATE TABLE DRIVER(
-#    NAME text,
-#    PHONE text,
-#    ADDRESS text,
-#    BTYPE text,
-#    ORG_NAME text,
-#    CAPACITY INT,
-#    STARTING text,
-#    ENDING text,
-#    PRICE INT,
-#    DTIME_H INT,
-#    DTIME_M INT,
-#    ATIME_H INT,
-#    ATIME_M INT
-# )'''
-# c.execute(driverd)
+#c.execute("DROP TABLE DRIVER")
+driverd = '''CREATE TABLE IF NOT EXISTS DRIVER(
+   NAME text,
+   PHONE text,
+   ADDRESS text,
+   BTYPE text,
+   ORG_NAME text,
+   CAPACITY INT,
+   STARTING text,
+   ENDING text,
+   PRICE INT,
+   DTIME_H INT,
+   DTIME_M INT,
+   ATIME_H INT,
+   ATIME_M INT,
+   DDAY INT,
+   DMONTH text,
+   DYEAR INT
+)'''
+c.execute(driverd)
 c.execute('SELECT * FROM DRIVER')
 print(c.fetchall())
 def startbar():
@@ -54,19 +57,39 @@ def intermediate():
 	driver = Button(root, text = "Add A Driver", bg = 'Red', fg = 'green', font = 18,command=adddriver)
 	driver.config(height = 1, width = 1)
 	driver.pack(fill = X, ipady = 20)
-	passenger = Button(root,text = "Book A Ride", bg = 'green',fg='red', font = 18)
+	passenger = Button(root,text = "Book A Ride", bg = 'green',fg='red', font = 18,command = customer)
 	passenger.config(height = 1, width = 1)
 	passenger.pack(fill = X, ipady = 20)
+
+def customer():
+	ping = Toplevel()
+	ping.geometry("500x500")
+	ping.title("Search A Ride")
+	heading = Label(ping,text ="Ajay Booking Services",bg = "green",fg ='blue', font=("Times New Roman", 24))
+	heading.grid(row = 0, columnspan = 4, sticky = E)
+	bus2 = PhotoImage(file = "bus.png")
+	bus2 = bus2.subsample(2,2)
+	photo = Label(ping,image = bus2)
+	photo.grid(row = 2, columnspan = 4)
+	Label(ping, text = "Source").grid(row = 4, column =0)
+	cussource = Entry(ping)
+	cussource.grid(row = 4, column = 1)
+	Label(ping, text = "Destination").grid(row = 5, column = 0)
+	cusdestination = Entry(ping)
+	cusdestination.grid(row = 5, column = 1)
+
+
+
 
 def printer(ping,namebox,addressbox,phonebox,ogname,bustypes,capacity_box,from_box,destination,pricet,time_h,time_m,atime_h,atime_m):
 	if not((len(bustypes.get())) and len(ogname.get()) and len(capacity_box.get()) and len(from_box.get()) and len(destination.get()) and len(pricet.get()) and len(time_h.get()) and len(time_m.get()) and len(atime_h.get()) and len(atime_m.get())):
 		messagebox.showerror("Details Not Filled", "Please Check All the Boxes and enter correct details")
 		ping.destroy()
 	else:
-		enterdate = [(str(namebox.get()),str(phonebox.get()),str(addressbox.get()),str(bustypes.get()),str(ogname.get()),int(capacity_box.get()),str(from_box.get()),str(destination.get()),int(pricet.get()),int(time_h.get()),int(time_m.get()),int(atime_h.get()),int(atime_m.get())),(str(namebox.get()),str(phonebox.get()),str(addressbox.get()),str(bustypes.get()),str(ogname.get()),int(capacity_box.get()),str(from_box.get()),str(destination.get()),int(pricet.get()),int(time_h.get()),int(time_m.get()),int(atime_h.get()),int(atime_m.get()))]
-		print(enterdate)
+		enterdate = [(str(namebox.get()),str(phonebox.get()),str(addressbox.get()),str(bustypes.get()),str(ogname.get()),int(capacity_box.get()),str(from_box.get()),str(destination.get()),int(pricet.get()),int(time_h.get()),int(time_m.get()),int(atime_h.get()),int(atime_m.get()))]
+		#print(enterdate)
 		c.executemany("INSERT INTO DRIVER VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",enterdate)
-		c
+		conn.commit()
 		messagebox.showinfo("Succesful", "Your Details have Been Added By Us!")
 		ping.destroy()
 
@@ -118,6 +141,14 @@ def driverinfo(ping,namebox,addressbox,phonebox):
 	atime_m = ttk.Combobox(ping, width = 27, textvariable = amtime) 
 	atime_m.grid(row = 17, column = 1)
 	atime_m['values']=['00','15','30','45']
+	Label(ping,text = "Departure Date(Month): ").grid(row = 18, column =0)
+	dmonth = ttk.Combobox(ping, width = 27)
+	dmonth['values'] = ['January', 'February', 'March', 'April', 'May', 'June', 'July','August', 'September', 'October', 'November', 'December']
+	dmonth.grid(row = 18, column = 1)
+	month = str(dmonth.get())
+	Label(ping,text = "Year").grid(row = 19, column =0)
+	dyear = Entry(ping)
+	dyear.grid(row =19, column = 1)
 	testb = Button(ping, text = "OK", command = lambda :printer(ping,namebox,addressbox,phonebox,ogname,bustypes,capacity_box,from_box,destination,pricet,time_h,time_m,atime_h,atime_m))
 	testb.grid(row = 19, column =0)
 
@@ -148,7 +179,7 @@ if __name__=="__main__":
 		ping.title("Adding A Bus Operator")
 		ping.geometry("400x450")
 		heading = Label(ping,text ="Ajay Booking Services",bg = "green",fg ='blue', font=("Times New Roman", 24))
-		heading.grid(row = 0, columnspan = 4)
+		heading.grid(row = 0, columnspan = 4, stick = NSEW)
 		bus2 = PhotoImage(file = "bus.png")
 		bus2 = bus2.subsample(2,2)
 		photo = Label(ping,image = bus2)
